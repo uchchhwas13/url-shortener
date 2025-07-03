97c06aa17d2623f61f32b04dc862f01428722f40
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-03 13:37:30 +0600

    Updating homepage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouter');
 const { connectToDatabase } = require('./connection');
 const URL = require('./models/url');
 
@@ -17,7 +18,10 @@ app.set('views', path.resolve('./views'));
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/url', urlRoute);
+app.use('/', staticRoute);
 
 app.get('/list', async (req, res) => {
   const urls = await URL.find({});
