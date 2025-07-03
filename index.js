926b682ccfa28eec1225df871c2c1819aa5629d8
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-03 09:38:14 +0600

    Adding middleware for json data and adding route

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const urlRoute = require('./routes/url');
 const {connectToDatabase} = require('./connection');
+
 const app = express();
 const port = 8000;
 
 connectToDatabase('mongodb://localhost:27017/short-url')
+
+app.use(express.json());
+app.use("/url", urlRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
