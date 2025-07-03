a66b285344d788e41b0e603b946f2aefcdc89eee
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-03 12:57:51 +0600

    Updating home.ejs file for rendering

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const URL = require('./models/url');
 const app = express();
 const port = 8000;
 
+// Connect to MongoDB
 connectToDatabase('mongodb://localhost:27017/short-url');
 
+// Set up EJS as the view engine
+// and specify the views directory
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
+
 
 app.use(express.json());
 app.use('/url', urlRoute);
@@ -18,7 +22,9 @@ app.use('/url', urlRoute);
 app.get('/list', async (req, res) => {
   const urls = await URL.find({});
   console.log('List of URLs:', urls);
-  return res.render("home");
+  return res.render("home", {
+    urls: urls
+  });
 });
 
 app.listen(port, () =>
