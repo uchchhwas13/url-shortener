bfaaa7e03f0fd41b3c972dd9270e1bc9d4fa709b
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-03 10:52:57 +0600

    Adding analytics get api

--- controllers/url.js
+++ controllers/url.js
@@ -1,22 +1,38 @@
-const {nanoid} = require('nanoid');
+const { nanoid } = require('nanoid');
 const URL = require('../models/url');
 
 async function handleGenerateNewShortUrl(req, res) {
-    if (!req.body.url) {
-        return res.status(400).json({error: 'Required URL is missing'});
-    }  
-    const shortID = nanoid(8); 
-    await URL.create({
-        shortId: shortID,
-        requiredUrl: req.body.url,
-        visitHistory: []
-    });
+  if (!req.body.url) {
+    return res.status(400).json({ error: 'Required URL is missing' });
+  }
+  const shortID = nanoid(8);
+  await URL.create({
+    shortId: shortID,
+    requiredUrl: req.body.url,
+    visitHistory: [],
+  });
 
-    return res.status(201).json({
-        shortId: shortID
+  return res.status(201).json({
+    shortId: shortID,
+  });
+}
+
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOne({ shortId: shortId });
+  console.log("Request for analytics:", entry);
+  if (!entry) {
+    return res.status(404).json({ error: 'Short URL not found' });
+  }
+  return res
+    .status(200)
+    .json({
+      totalClicks: entry.visitHistory.length,
+      analytics: entry.visitHistory
     });
 }
 
 module.exports = {
-    handleGenerateNewShortUrl
+  handleGenerateNewShortUrl,
+  handleGetAnalytics
 };
--- routes/url.js
+++ routes/url.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const {handleGenerateNewShortUrl} = require('../controllers/url');
+const {handleGenerateNewShortUrl, handleGetAnalytics} = require('../controllers/url');
 const router = express.Router();
 
 router.post('/', handleGenerateNewShortUrl);
+router.get('/analytics/:shortId', handleGetAnalytics);
 module.exports = router;
