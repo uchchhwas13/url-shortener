6c0b9cc29773592a8556d81b0de46bd1aed54d06
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-03 09:54:43 +0600

    adding get request for shortId and redirecting to the correct url

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 
 const urlRoute = require('./routes/url');
-const {connectToDatabase} = require('./connection');
+const { connectToDatabase } = require('./connection');
+const URL = require('./models/url');
 
 const app = express();
 const port = 8000;
 
-connectToDatabase('mongodb://localhost:27017/short-url')
+connectToDatabase('mongodb://localhost:27017/short-url');
 
 app.use(express.json());
-app.use("/url", urlRoute);
+app.use('/url', urlRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.get('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId;
+  // Here you would typically look up the URL by shortId in your database
+  // and redirect to the original URL.
+  const entry = await URL.findOneAndUpdate(
+    { shortId: shortId },
+    {
+      $push: {
+        visitHistory: { timeStamp: Date.now() },
+      },
+    }
+  );
+  res.redirect(entry.requiredUrl);
 });
+
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
