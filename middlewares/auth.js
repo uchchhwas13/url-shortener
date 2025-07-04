19e1ab354b3f778119fbf8b8efbbe10f2dafc90c
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-04 12:16:03 +0600

    Adding restrictToLoggedInUserOnly middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+const { getUser } = require('../service/auth');
+/**
+ * Middleware to restrict access to logged-in users only.
+ * If the user is not logged in, they will be redirected to the login page.
+ */
+async function restrictToLoggedInUserOnly(req, res, next) {
+  const userUid = req.cookies.uid;
+  if (!userUid) return res.redirect('/login');
+  const user = await getUser(userUid);
+  if (!user) return res.redirect('/login');
+  req.user = user;
+  next();
+}
+
+module.exports = {
+  restrictToLoggedInUserOnly,
+};
