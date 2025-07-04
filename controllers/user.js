a3f7c1402a7d30af5833b356fd6e6a89b3797094
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-04 11:42:06 +0600

    Adding handleUserLogin function

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,34 @@
 const User = require('../models/user');
 
 async function handleUserSignup(req, res) {
-    const { name, email, password } = req.body;
-    
-    if (!name || !password || !email) {
-        return res.status(400).json({ error: 'Nme, password and email are required' });
-    }
-    
-    await User.create({
-        name: name,
-        email: email,
-        password: password,
+  const { name, email, password } = req.body;
+
+  if (!name || !password || !email) {
+    return res
+      .status(400)
+      .json({ error: 'Nme, password and email are required' });
+  }
+
+  await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
+  return res.ridirect('/');
+}
+
+async function handleUserLogin(req, res) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
+  if (!user) {
+    return res.render('login', {
+      error: 'Invalid email or password',
     });
-    return res.render("home");
+  }
+  return res.ridirect('/');
 }
 
 module.exports = {
-    handleUserSignup
+  handleUserSignup,
+  handleUserLogin
 };
