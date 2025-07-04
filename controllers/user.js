fbf0f7a28e328560a35c129886be79b270ad1b3e
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-04 12:08:37 +0600

    Saving uid as cookie and saving logged in user in map

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
+const {v4: uuidv4} = require('uuid');
 const User = require('../models/user');
+const {setUser} = require("../service/auth");
 
 async function handleUserSignup(req, res) {
   const { name, email, password } = req.body;
@@ -25,6 +27,9 @@ async function handleUserLogin(req, res) {
       error: 'Invalid email or password',
     });
   }
+  const sessionId = uuidv4();
+  setUser(sessionId, user);
+  res.cookie('uid', sessionId);
   return res.redirect('/');
 }
 
---
+++ service/auth.js
@@ -0,0 +1,14 @@
+const sessionIdToUserMap = new Map();
+
+function setUser(sessionId, user) {
+    sessionIdToUserMap.set(sessionId, user);
+}   
+
+function getUser(sessionId) {
+    return sessionIdToUserMap.get(sessionId);
+}   
+
+module.exports = {
+    setUser,
+    getUser
+};
